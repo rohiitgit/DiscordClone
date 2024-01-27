44d837f39a72b59f95f013d319ba9bd29b4c0fa3
rohiitgit <rohitcodes03@gmail.com>
2024-01-27 19:16:36 +0530

    added authentication and themes

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 
 const state = true;
@@ -5,7 +7,10 @@ const state = true;
 export default function Home() {
   return (
     <div>
-    This is a protected route.
+    <UserButton 
+    afterSignOutUrl="/"
+    />
+    <ModeToggle />
     </div>
   );
 }
